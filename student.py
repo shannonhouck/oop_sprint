dd89770915f3b0590177ea46af693b5b5703d43e
Shannon Houck <shannonhouck@shannon-book.local>
2017-08-02 09:58:44 -0400

    Added functionality

--- student.py
+++ student.py
@@ -6,10 +6,11 @@ A module for handling school students related information
 class Student(object):
     """Basic student class"""
 
-    def __init__(self, name, ID, GPA):
+    def __init__(self, name, ID, GPA, status):
         self.name = name
         self._GPA = GPA     # single underscore == protected
         self.__ID = ID      # double underscore == private
+        self.status = status
 
     def get_GPA(self):
         return self._GPA
@@ -19,3 +20,29 @@ class Student(object):
 
     def __str__(self):
         return "Student Name " + self.name
+
+
+class GradStudent(Student):
+    """A graduate student which inherits stuff from the student class"""
+
+    def __init__(self, name, ID, GPA):
+        super().__init__(name, ID, GPA, 'Grad')
+
+    def is_pass(self):
+        return self.get_GPA() >= 3.0
+
+    def set_advisor(self, advisor):
+        self._advisor = advisor
+
+    def get_advisor(self):
+        return self._advisor
+
+
+class UndergradStudent(Student):
+    """An undergraduate student which inherits stuff from the student class"""
+
+    def __init__(self, name, ID, GPA):
+        super().__init__(name, ID, GPA, 'Undergrad')
+
+    def is_pass(self):
+        return self.get_GPA() >= 2.0
--- tests/test_student.py
+++ tests/test_student.py
@@ -1,34 +1,34 @@
 import pytest
-# from student import Student, GradStudent, UndergradStudent
-from student import Student
+from student import Student, GradStudent, UndergradStudent
 
 
-s = Student(name='Doaa', ID='453535', GPA=2.3)
-# s1 = GradStudent(name='Joe Smith', ID='453535', GPA=2.7)
-# s2 = UndergradStudent(name='Sally Smith', ID='453535', GPA=2.7)
+s = Student(name='Doaa', ID='453535', GPA=2.3, status='')
+s1 = GradStudent(name='Joe Smith', ID='453535', GPA=2.7)
+s2 = UndergradStudent(name='Sally Smith', ID='453535', GPA=2.7)
 
 
 def test_name():
-    # assert s1.name == 'Joe Smith'
-    # assert s2.name == 'Sally Smith'
+    assert s1.name == 'Joe Smith'
+    assert s2.name == 'Sally Smith'
     assert s.name == 'Doaa'
 
 
 def test_GPA():
-    # assert s1.get_GPA() == 2.7
-    # assert s2.get_GPA() == 2.7
+    assert s1.get_GPA() == 2.7
+    assert s2.get_GPA() == 2.7
     assert s.get_GPA() == 2.3
 
+
 def test_pass():
-    # assert not s1.is_pass()
-    # assert s2.is_pass()
+    assert not s1.is_pass()
+    assert s2.is_pass()
     pass
 
 
 def test_advisor():
-    # advisor = 'Shaffer'
-    # s1.set_advisor(advisor)
-    # assert s1.get_advisor() == advisor
+    advisor = 'Shaffer'
+    s1.set_advisor(advisor)
+    assert s1.get_advisor() == advisor
     pass
 
 
